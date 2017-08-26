67c67820f8f4b116a6579d62033822be936b0b32
Massimo Biagioli <biagiolimassimo@gmail.com>
2017-08-26 09:26:01 +0200

    finish weather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
-const API_KEY = "b8b0e2385e43517564aec143e2f7bfb6";
+import axios from 'axios';
+
+const API_KEY = 'b8b0e2385e43517564aec143e2f7bfb6';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
---
+++ src/components/google_map.js
@@ -0,0 +1,42 @@
+/* VERSIONE CON COMPONENTI - DOESN'T WORK !!!
+==============================================
+import React from 'react';
+import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
+
+export default (props) => {
+  return (
+    <GoogleMapLoader
+      containerElement={ <div style={{height: '100%'}} /> }
+      googleMapElement={
+        <GoogleMap 
+          defaultZoom={12}
+          defaultCenter={{lat: props.lat, lng: props.lon }}
+        />
+      }
+    />
+  );
+}
+*/
+
+/* VERSIONE SENZA COMPONENTI 
+==============================================*/
+import React, { Component } from 'react';
+
+class GoogleMap extends Component {
+  componentDidMount() {
+    new google.maps.Map(this.refs.map, {
+      zoom: 12,
+      center: {
+        lat: this.props.lat,
+        lng: this.props.lon
+      }
+    });
+  }
+  
+  render() {
+    // this.refs.map
+    return <div ref="map" />
+  }
+}
+
+export default GoogleMap;
